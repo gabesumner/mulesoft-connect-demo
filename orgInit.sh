cce5eeac2700f2fc42b8157ae0cdd4a19131a063
Gabe Sumner <gsumner@gsumner-ltm7.internal.salesforce.com>
2019-06-21 09:56:29 -0700

    Added auto theme activation.

--- orgInit.sh
+++ orgInit.sh
@@ -6,5 +6,6 @@ sfdx force:user:permset:assign -n electron
 sfdx force:apex:execute -f scripts/generateAccounts.apex
 sfdx force:apex:execute -f scripts/createReema.apex
 
+sfdx shane:theme:activate -n Electron
 sfdx force:org:open
 sfdx shane:user:password:set -g User -l User -p sfdx1234
